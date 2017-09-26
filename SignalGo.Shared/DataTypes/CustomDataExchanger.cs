f36ddc0b876b1347c38af3b407f74f730ba4318c
ali yousefi <ali.visual.studio@gmail.com>
2017-09-26 11:43:41 +0330

    add types for custom data exchanger

--- SignalGo.Shared/DataTypes/CustomDataExchanger.cs
+++ SignalGo.Shared/DataTypes/CustomDataExchanger.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Text;
+using System.Linq;
 
 namespace SignalGo.Shared.DataTypes
 {
@@ -35,10 +37,59 @@ namespace SignalGo.Shared.DataTypes
             Type = type;
             Properties = properties;
         }
+
         /// <summary>
-        /// 
+        /// default constructor for data exchanger
         /// </summary>
-        /// <returns></returns>
+        /// <param name="type">type of your class to ignore or take properties for serialize</param>
+        /// <param name="properties">list of types you want to take methods of that types</param>
+        public CustomDataExchanger(Type type, params Type[] properties)
+        {
+            Type = type;
+            Properties = GetMethods(properties).ToArray();
+        }
+
+        /// <summary>
+        /// get list of methods of type
+        /// </summary>
+        /// <param name="types">your types</param>
+        /// <returns>list of methods names</returns>
+        public static List<string> GetMethods(Type[] types)
+        {
+            List<string> result = new List<string>();
+            foreach (var serviceType in types)
+            {
+                foreach (var item in serviceType.GetInterfaces())
+                {
+                    result.AddRange(item.GetMethods().Select(x => x.Name));
+                }
+#if (NETSTANDARD1_6 || NETCOREAPP1_1)
+                var parent = serviceType.GetTypeInfo().BaseType;
+#else
+            var parent = serviceType.BaseType;
+#endif
+                while (parent != null)
+                {
+                    result.AddRange(parent.GetMethods().Select(x => x.Name));
+                    
+                    foreach (var item in parent.GetInterfaces())
+                    {
+                        result.AddRange(item.GetMethods().Select(x => x.Name));
+                    }
+#if (NETSTANDARD1_6 || NETCOREAPP1_1)
+                    parent = parent.GetTypeInfo().BaseType;
+#else
+                    parent = parent.BaseType;
+#endif
+                }
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// you can customize enable and disable ignorable
+        /// </summary>
+        /// <returns>if you return false system force skip to ignore property</returns>
         public virtual bool IsEnabled(object client, object server, string propertyName, Type type)
         {
             return true;
