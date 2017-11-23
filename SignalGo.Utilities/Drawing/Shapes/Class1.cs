7e2d044ecf3bdc5c2a86167c1a9dbbcc2883cc7e
Ali Yousefi <ali.visual.studio@gmail.com>
2017-11-23 11:04:33 +0330

    initialize signalgo utilities

---
+++ SignalGo.Utilities/Drawing/Shapes/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalGo.Drawing.Shapes
+{
+    class Class1
+    {
+    }
+}
