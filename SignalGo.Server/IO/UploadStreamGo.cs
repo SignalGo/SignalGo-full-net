90921bdaf46b2ef72609e35e58b7b164a19f0a21
Ali Yousefi <ali.visual.studio@gmail.com>
2017-12-02 20:47:21 +0330

    fix for upload stream

---
+++ SignalGo.Server/IO/UploadStreamGo.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SignalGo.Server.IO
+{
+    //internal class UploadStreamGo : Stream
+    //{
+    //    NetworkStream CurrentStream { get; set; }
+
+    //    public UploadStreamGo(NetworkStream currentStream)
+    //    {
+    //        CurrentStream = currentStream;
+    //    }
+
+    //    long _Length;
+
+    //    long _Position;
+    //    public override bool CanRead => CurrentStream.CanRead;
+
+    //    public override bool CanSeek => CurrentStream.CanSeek;
+
+    //    public override bool CanWrite => CurrentStream.CanWrite;
+
+    //    public override long Length => _Length;
+
+    //    public override long Position { get => _Position; set => _Position = value; }
+
+    //    public override void Flush()
+    //    {
+    //        CurrentStream.Flush();
+    //    }
+
+    //    public void SetLengthOfBase(long length)
+    //    {
+    //        _Length = length;
+    //    }
+
+    //    public bool IsFinished
+    //    {
+    //        get
+    //        {
+    //            return _Length == _Position;
+    //        }
+    //    }
+
+    //    public override int Read(byte[] buffer, int offset, int count)
+    //    {
+    //        int readCount = CurrentStream.Read(buffer, offset, count);
+    //        Position += readCount;
+    //        return readCount;
+    //    }
+
+    //    public override long Seek(long offset, SeekOrigin origin)
+    //    {
+    //        return CurrentStream.Seek(offset, origin);
+    //    }
+
+    //    public override void SetLength(long value)
+    //    {
+    //        CurrentStream.SetLength(value);
+    //    }
+
+    //    public override void Write(byte[] buffer, int offset, int count)
+    //    {
+    //        CurrentStream.Write(buffer, offset, count);
+    //    }
+    //}
+}
