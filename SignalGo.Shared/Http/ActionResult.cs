eab8fa3ff43006aa6869d7f05f1448001c6bcc49
Ali Yousefi <ali.visual.studio@gmail.com>
2018-01-29 13:25:34 +0330

    add custom setting for clients in OperationContext

--- SignalGo.Shared/Http/ActionResult.cs
+++ SignalGo.Shared/Http/ActionResult.cs
@@ -5,6 +5,18 @@ using System.Text;
 
 namespace SignalGo.Shared.Http
 {
+    public static class ActionResultExtension
+    {
+        public static ActionResult ToActionResult(this object data)
+        {
+            if (data == null)
+                return null;
+            if ((data as ActionResult) != null)
+                return (ActionResult)data;
+            return new ActionResult(data);
+        }
+    }
+
     public class ActionResult
     {
         public object Data { get; private set; }
