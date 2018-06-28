7925907a07dfc8eb042db8237b44fcf075f99272
Ali Yousefi <ali.visual.studio@gmail.com>
2018-06-28 13:27:28 +0430

    start server manager project

---
+++ SignalGo.ServerManager/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SignalGo.ServerManager
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoad_Click(object sender, RoutedEventArgs e)
+        {
+            AppDomain newDomainName = AppDomain.CreateDomain("newDomainName");
+
+            try
+            {
+                Assembly testLibrary = newDomainName.Load(LibraryName);
+            }
+            finally
+            {
+                AppDomain.Unload(newDomainName);
+            }
+        }
+    }
+}
