ee22462ea84d1871991c3579e68b3801b60ed354
Ali Yousefi <ali.visual.studio@gmail.com>
2018-09-25 00:21:29 +0330

    merge version4 to master

---
+++ SignalGoTest/Models/DataExchangerTestModel.cs
@@ -0,0 +1,51 @@
+﻿using SignalGo.Shared.DataTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalGoTest.Models
+{
+    public class UserInfoTest
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        [CustomDataExchanger(ExchangeType = CustomDataExchangerType.Ignore, LimitationMode = LimitExchangeType.OutgoingCall)]
+        public string Password { get; set; }
+        public int Age { get; set; }
+        [CustomDataExchanger(ExchangeType = CustomDataExchangerType.Ignore, LimitationMode = LimitExchangeType.IncomingCall)]
+        public List<PostInfoTest> PostInfoes { get; set; }
+        public List<RoleInfoTest> RoleInfoes { get; set; }
+        [CustomDataExchanger(ExchangeType = CustomDataExchangerType.Ignore, LimitationMode = LimitExchangeType.Both)]
+        public PostInfoTest LastPostInfo { get; set; }
+    }
+
+    [CustomDataExchanger("Title", "Text", ExchangeType = CustomDataExchangerType.TakeOnly, LimitationMode = LimitExchangeType.IncomingCall)]
+    [CustomDataExchanger("Id", "Title", "Text", ExchangeType = CustomDataExchangerType.TakeOnly, LimitationMode = LimitExchangeType.OutgoingCall)]
+    public class PostInfoTest
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public string PostSecurityLink { get; set; }
+        [CustomDataExchanger(ExchangeType = CustomDataExchangerType.Ignore, LimitationMode = LimitExchangeType.Both)]
+        public UserInfoTest User { get; set; }
+        public RoleInfoTest PostRoleToSee { get; set; }
+    }
+
+    public enum RoleTypeTest
+    {
+        Normal,
+        Admin,
+        Editor,
+        Viewer
+    }
+
+    public class RoleInfoTest
+    {
+        public int Id { get; set; }
+        public RoleTypeTest Type { get; set; }
+        public UserInfoTest User { get; set; }
+    }
+}
---
+++ SignalGoTest/Models/TestServerStreamModel.cs
@@ -0,0 +1,23 @@
+﻿using SignalGo.Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalGoTest.Models
+{
+    public class TestServerStreamModel : ITestServerStreamModel
+    {
+        public StreamInfo<string> DownloadImage(string name, TestStreamModel testStreamModel)
+        {
+            return new StreamInfo<string>() { Data = "hello return", Length = 4, Stream = new MemoryStream(new byte[] { 2, 5, 8, 9 }) };
+        }
+
+        public string UploadImage(string name, StreamInfo streamInfo, TestStreamModel testStreamModel)
+        {
+            return null;
+        }
+    }
+}
