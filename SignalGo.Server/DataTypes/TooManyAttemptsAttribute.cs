528715694366f91d6ccd525f46b1b6674ea32554
Ali Yousefi <ali.visual.studio@gmail.com>
2019-01-18 04:14:31 +0330

    work on too many attemps

---
+++ SignalGo.Server/DataTypes/TooManyAttemptsAttribute.cs
@@ -0,0 +1,55 @@
+﻿using SignalGo.Server.Models;
+using System;
+using System.Collections.Concurrent;
+
+namespace SignalGo.Server.DataTypes
+{
+    internal class TooManyAttemptsInfo
+    {
+        /// <summary>
+        /// last requested datetime
+        /// </summary>
+        public DateTime LastRequestedDateTime { get; set; }
+        /// <summary>
+        /// count of called each time
+        /// </summary>
+        public int Count { get; set; }
+    }
+
+    /// <summary>
+    /// check too many attempts system
+    /// limit client too call methods each times
+    /// </summary>
+    public class TooManyAttemptsAttribute : Attribute
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="duration">duration of limit time each call methods</param>
+        public TooManyAttemptsAttribute(TimeSpan duration)
+        {
+            Duration = duration;
+        }
+
+        /// <summary>
+        /// duration of limit time each call methods
+        /// </summary>
+        public TimeSpan Duration { get; set; } = new TimeSpan(0, 0, 1);
+
+        /// <summary>
+        /// last request date time each ip address
+        /// </summary>
+        internal ConcurrentDictionary<long, TooManyAttemptsInfo> LastRequestedEachIpAddress { get; set; } = new ConcurrentDictionary<long, TooManyAttemptsInfo>();
+        /// <summary>
+        /// check id client has permissions
+        /// </summary>
+        /// <param name="clientInfo"></param>
+        /// <returns></returns>
+        public virtual bool HasPermissions(ClientInfo clientInfo)
+        {
+            return true;
+        }
+
+
+    }
+}
---
+++ SignalGo.Shared/Helpers/MathHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SignalGo.Shared.Helpers
+{
+    public static class MathHelper
+    {
+        public static bool ByteArrayCompare(byte[] bytes1, byte[] bytes2)
+        {
+            if (bytes1.Length != bytes2.Length)
+                return false;
+
+            for (int i = 0; i < bytes1.Length; i++)
+                if (bytes1[i] != bytes2[i])
+                    return false;
+
+            return true;
+        }
+    }
+}
