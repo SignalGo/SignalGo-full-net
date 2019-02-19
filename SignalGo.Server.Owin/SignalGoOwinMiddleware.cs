d31e22a38904f8a09d718f56632a4ab267662d16
Ali Yousefi <ali.visual.studio@gmail.com>
2019-02-19 20:54:07 +0330

    fix signalgo add reference update or generate

--- SignalGo.Server.Owin/SignalGoOwinMiddleware.cs
+++ SignalGo.Server.Owin/SignalGoOwinMiddleware.cs
@@ -37,7 +37,7 @@ namespace SignalGo.Server.Owin
 
             string serviceName = context.Request.Uri.PathAndQuery.Split(new string[] { "/" }, StringSplitOptions.RemoveEmptyEntries).FirstOrDefault();
             bool isWebSocketd = context.Request.Headers.ContainsKey("Sec-WebSocket-Key");
-            if (!BaseProvider.ExistService(serviceName, CurrentServerBase) && !isWebSocketd)
+            if (!BaseProvider.ExistService(serviceName, CurrentServerBase) && !isWebSocketd && !context.Request.Headers.ContainsKey("signalgo") && !context.Request.Headers.ContainsKey("signalgo-servicedetail") && context.Request.Headers["content-type"] != "SignalGo Service Reference")
                 return Next.Invoke(context);
 
             OwinClientInfo owinClientInfo = new OwinClientInfo(CurrentServerBase);
