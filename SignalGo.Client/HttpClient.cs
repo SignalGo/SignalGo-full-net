c3a12965c7fd84921dba65de438fdc1438c75b55
Ali Yousefi <ali.visual.studio@gmail.com>
2019-07-22 13:09:32 +0430

    fix await connection

--- SignalGo.Client/HttpClient.cs
+++ SignalGo.Client/HttpClient.cs
@@ -9,6 +9,7 @@ using System.Net;
 using System.Net.Security;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace SignalGo.Client
@@ -235,7 +236,25 @@ namespace SignalGo.Client
         {
             string newLine = TextHelper.NewLine;
             Uri uri = new Uri(url);
-            TcpClient tcpClient = new TcpClient(uri.Host, uri.Port);
+            TcpClient tcpClient = new TcpClient();
+//#if (__ANDROID__)
+//            var cancelSource = new CancellationTokenSource();
+//            cancelSource.Token.ThrowIfCancellationRequested();
+//            _ = Task.Run(async () =>
+//                {
+//                    for (int i = 0; i < 6; i++)
+//                    {
+//                        await Task.Delay(1000);
+//                    }
+//                    cancelSource.Cancel();
+//                });
+//            await Task.Run(async () =>
+//            {
+//                await tcpClient.ConnectAsync(uri.Host, uri.Port);
+//            }, cancelSource.Token);
+//#else
+            await tcpClient.ConnectAsync(uri.Host, uri.Port);
+//#endif
             try
             {
                 if (streamInfo != null && (!streamInfo.Length.HasValue || streamInfo.Length <= 0))
@@ -364,5 +383,5 @@ namespace SignalGo.Client
             }
         }
 #endif
-    }
+        }
 }
