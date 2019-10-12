7e32d1296180192f7badedab44db92cccbba9ee5
@ali.visual.studio <ali.visual.studio@gmail.com>
2019-10-12 17:29:33 +0330

    fix soap service bug

---
+++ SignalGo.Client/IO/TimeoutWebClient.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+namespace SignalGo.Client.IO
+{
+    public class TimeoutWebClient : WebClient
+    {
+        protected override WebRequest GetWebRequest(Uri uri)
+        {
+            WebRequest w = base.GetWebRequest(uri);
+            w.Timeout = 40 * 1000;
+            return w;
+        }
+    }
+}
