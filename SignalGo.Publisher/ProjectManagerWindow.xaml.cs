f86328992de07ff529fded9ff9c54cbbf8ce9ed9
saeed rezai <mrgrayhatt@hotmail.com>
2020-07-15 01:11:40 +0430

    Added Display RamUsage And Cache/Memory Clean Feature to free publisher mem if need. Add AccessControl Button To Disable Password Asking When Executing Command's On Server's.

--- SignalGo.Publisher/ProjectManagerWindow.xaml.cs
+++ SignalGo.Publisher/ProjectManagerWindow.xaml.cs
@@ -1,19 +1,18 @@
-﻿using MvvmGo.ViewModels;
+﻿using System;
+using System.IO;
+using System.Windows;
+using MvvmGo.ViewModels;
 using SignalGo.Publisher.Models;
-using SignalGo.Publisher.Services;
 using SignalGo.Publisher.ViewModels;
 using SignalGo.Publisher.Views;
-using SignalGo.Shared;
-using System;
-using System.IO;
 using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Navigation;
+using SignalGo.Shared;
 
 namespace SignalGo.Publisher
 {
@@ -26,21 +25,72 @@ namespace SignalGo.Publisher
 
         public ProjectManagerWindow()
         {
-            //AsyncActions.InitializeUIThread();
             BaseViewModel.Initialize();
             BaseViewModel.RunOnUIAction = (x) =>
             {
                 Dispatcher.BeginInvoke(x);
             };
+            AsyncActions.InitializeUIThread();
             This = this;
             InitializeComponent();
             mainframe.Navigate(new FirstPage());
-            Closing += MainWindow_Closing;
-            //PublisherServiceProvider.Initialize();
-        }
-        private void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
-        {
+            Closing += (s, e) =>
+            {
+                try
+                {
+                    //File.Delete(UserSettingInfo.Current.UserSettings.CommandRunnerLogsPath);
+                }
+                catch { }
+            };
+            // this event execute code's after app loaded
+            Loaded += async (s, e) =>
+            {
+                    try
+                    {
+                        // check if application log files is for past, and then backup
+                        string backupPath = Path.Combine(Environment.CurrentDirectory, "AppBackups", "Logs");
+                        var fileDate = File.GetCreationTime(UserSettingInfo.Current.UserSettings.LoggerPath);
 
+                        if (fileDate.Date < DateTime.Now.Date)
+                        {
+                            // backup logs to a new file with it's date
+                            string outFileName = $"{Path.GetFileNameWithoutExtension(UserSettingInfo.Current.UserSettings.LoggerPath)}{fileDate: _MMddyyyy}.log";
+                            File.Move(UserSettingInfo.Current.UserSettings.LoggerPath,
+                                Path.Combine(backupPath, outFileName), false);
+                            File.Create(UserSettingInfo.Current.UserSettings.LoggerPath).Dispose();
+                        }
+                        #region to multiple backup
+                        //Dictionary<string, DateTime> logsInfo = new Dictionary<string, DateTime>();
+
+                        //logsInfo.Add(UserSettingInfo.Current.UserSettings.CommandRunnerLogsPath, File.GetCreationTime(UserSettingInfo.Current.UserSettings.CommandRunnerLogsPath));
+                        //logsInfo.Add(UserSettingInfo.Current.UserSettings.LoggerPath, File.GetCreationTime(UserSettingInfo.Current.UserSettings.LoggerPath));
+
+                        //if (!Directory.Exists(backupPath))
+                        //    Directory.CreateDirectory(backupPath);
+
+                        //foreach (var item in logsInfo)
+                        //{
+                        //    //var day = item.Value.Date.Day;
+                        //    if (item.Value.Date != DateTime.Now.Date)
+                        //    {
+                        //        // backup logs to a new file with it's date
+                        //        string outFileName = $"{Path.GetFileNameWithoutExtension(item.Key)}{item.Value: _MMddyyyy}.log";
+                        //        try
+                        //        {
+                        //            File.Copy(item.Key,
+                        //                Path.Combine(backupPath, outFileName), true);
+                        //        }
+                        //        catch { }
+                        //    }
+
+                        //}
+                        #endregion
+                    }
+                    catch (Exception ex)
+                    {
+
+                    }
+            };
         }
 
         private void MainFrame_Navigating(object sender, NavigatingCancelEventArgs e)
@@ -110,13 +160,5 @@ namespace SignalGo.Publisher
             }
         }
 
-        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
-        {
-            try
-            {
-                File.Delete(UserSettingInfo.Current.UserSettings.CommandRunnerLogsPath);
-            }
-            catch { }
-        }
     }
 }
