2de31b856b9ddd5868dcfc951e3743fcac8fde64
Ali Yousefi <ali.visual.studio@gmail.com>
2021-05-01 14:36:35 +0430

    add CanAcceptClient for manage client acceptable in server side

--- SignalGo.Server/ServiceManager/Versions/IServerDataProvider.cs
+++ SignalGo.Server/ServiceManager/Versions/IServerDataProvider.cs
@@ -6,6 +6,7 @@ namespace SignalGo.Server.ServiceManager.Versions
 {
     public interface IServerDataProvider
     {
+        bool CanAcceptClient { get; set; }
         void Start(ServerBase serverBase, int port);
         ClientInfo CreateClientInfo(bool isHttp, TcpClient tcpClient, PipeNetworkStream stream);
         string GetInformation();
